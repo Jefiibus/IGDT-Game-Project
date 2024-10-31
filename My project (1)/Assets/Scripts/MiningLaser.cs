c83d9cb930e047ff2afac9116e2c62ddb4bd8c51
polareal <haakon.helsko@hotmail.com>
2024-10-31 22:40:05 +0200

    Holy shit it's a fucking laser
    press m2

---
+++ My project (1)/Assets/Scripts/MiningLaser.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MiningLaser : MonoBehaviour
+{
+    public GameObject playerObj;
+    public LineRenderer line;
+    //public Transform lineEndPoint;
+    Vector2 lineDefaultPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(1))
+        {
+            InvokeRepeating("FireBeam",0,0);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            CancelInvoke("CancelBeam");
+        }
+    }
+    void FireBeam()
+    {
+        Vector2 playerPosition = playerObj.transform.position;
+        Vector2 lineEndPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Firing laser");
+        line.enabled = true;
+
+        line.SetPosition(0, playerPosition);
+        line.SetPosition(1, lineEndPoint);
+        
+        Debug.Log(lineEndPoint);
+    }
+    void CancelBeam()
+    {
+        line.enabled = false;
+        line.positionCount = 0;
+    }
+}
